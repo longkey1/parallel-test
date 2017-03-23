b2b9d8f2e0f532d32f8fba5adfa0f0a0c10658ab
longkey1 <longkey1@gmail.com>
2017-03-23 17:39:54 +0900

    Change test time & names

--- test.php
+++ test.php
@@ -2,5 +2,5 @@
 
 $name = !empty($argv[1]) ? $argv[1] : "noname";
 echo sprintf('%s %s start' . PHP_EOL, date('Y-m-d H:i:s'), $name);
-sleep(10);
+sleep(60);
 echo sprintf('%s %s end' . PHP_EOL, date('Y-m-d H:i:s'), $name);
